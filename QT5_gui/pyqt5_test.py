185e834d3b47990609437386a13df994e169cbcc
Mark Watson <markw@markwatson.com>
2020-02-19 17:00:54 -0700

    removed code experiment

--- QT5_gui/pyqt5_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication , QWidget
-
-app = QApplication(sys.argv)
-w = QWidget()
-w.resize(250, 150)
-w.setWindowTitle('Hy Qt Example')
-w.show()
-app.exec () 
